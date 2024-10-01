a70db97e764bc5dcbf5f603c691b78d0b20d5e1e
Bailon18 <paucarmontesbailon@gmail.com>
2024-10-01 00:12:56 -0500

    se actualizo

--- src/main/java/com/paucar/customer_ms/client/AccountClient.java
+++ src/main/java/com/paucar/customer_ms/client/AccountClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "account-ms")
+@FeignClient(name = "ACCOUNT-MS")
 public interface AccountClient {
 
     @GetMapping("/accounts/customer/{customerId}")
